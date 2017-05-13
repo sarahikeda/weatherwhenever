83555652d70fdae495746d206c1d7e6cf01b506c
Sarah Ing <sarahikeda@gmail.com>
2017-05-13 12:27:09 +0100

    Add function to determine browser coordinates

--- stylesheets/index.js
+++ stylesheets/index.js
@@ -1,6 +1,17 @@
-function getWeather() {
-    console.log('test');
-    $.getJSON('http://worksample-api.herokuapp.com/weather?q=Stockholm,SE&key=62fc4256-8f8c-11e5-8994-feff819cdc9f', function(data){
-      console.log(data);
-    });
+function locateUser(){
+  if (navigator.geolocation) {
+    (navigator.geolocation.getCurrentPosition(function(position){
+      var lat = (position.coords.latitude);
+      var lon = (position.coords.longitude);
+      getWeather(lat,lon);
+    }));
+  }
+};
+
+function getWeather(lat,lon) {
+  var weatherUrl = "http://worksample-api.herokuapp.com/weather?lat=" + lat + "&lon=" + lon + "&key=62fc4256-8f8c-11e5-8994-feff819cdc9f"
+  $.getJSON(weatherUrl, function(data){
+    var weatherConditions = [];
+    weatherConditions.push(data.weather[0].description);
+  });
 }
