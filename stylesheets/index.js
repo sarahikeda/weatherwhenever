af435b24a35f585e8a372fa4a890afafb194b6d5
Sarah Ing <sarahikeda@gmail.com>
2017-05-13 15:54:00 +0100

    Add function for parsing json

--- stylesheets/index.js
+++ stylesheets/index.js
@@ -8,15 +8,25 @@ function locateUser(){
   }
 };
 
+function createWeatherEndpoint(lat,lon) {
+  return "http://worksample-api.herokuapp.com/weather?lat=" + lat + "&lon=" + lon + "&key=62fc4256-8f8c-11e5-8994-feff819cdc9f"
+}
+
 function getWeather(lat,lon) {
   var weatherUrl = createWeatherEndpoint(lat, lon)
+  var cityConditions = {};
   $.getJSON(weatherUrl, function(data){
-    console.log(data);
-    var weatherConditions = [];
-    weatherConditions.push(data.weather[0].description);
+    cityConditions.city = data.name
+    cityConditions.temp =  data.main.temp;
+    cityConditions.humidity =  data.main.humidity;
+    cityConditions.weather = data.weather[0].main
+    displayWeather(cityConditions)
   });
 }
 
-function createWeatherEndpoint(lat,lon) {
-  return "http://worksample-api.herokuapp.com/weather?lat=" + lat + "&lon=" + lon + "&key=62fc4256-8f8c-11e5-8994-feff819cdc9f"
+function displayWeather(cityConditions) {
+  $.each(cityConditions, function(info, value){
+    $(".weather-form").append(value)
+
+  })
 }
