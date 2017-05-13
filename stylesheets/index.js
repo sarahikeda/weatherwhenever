b4cc7d994b9707f5e890759accc86b86f17acf77
Sarah Ing <sarahikeda@gmail.com>
2017-05-13 19:34:28 +0100

    Refactor to reduce redundancy

--- stylesheets/index.js
+++ stylesheets/index.js
@@ -3,7 +3,7 @@ function locateUser(){
     (navigator.geolocation.getCurrentPosition(function(position){
       var lat = (position.coords.latitude);
       var lon = (position.coords.longitude);
-      getWeather(lat,lon);
+      getCityConditions(lat,lon);
     }));
   }
 };
@@ -12,34 +12,38 @@ function createWeatherEndpoint(lat,lon) {
   return "http://worksample-api.herokuapp.com/weather?lat=" + lat + "&lon=" + lon + "&key=62fc4256-8f8c-11e5-8994-feff819cdc9f"
 }
 
-function getWeather(lat,lon) {
+function getCityConditions(lat,lon) {
   var weatherUrl = createWeatherEndpoint(lat, lon)
   var cityConditions = {};
   $.getJSON(weatherUrl, function(data){
-    cityConditions.city = data.name
     cityConditions.temp =  data.main.temp + "°C";
     cityConditions.weather = data.weather[0].main
-    displayWeather(cityConditions)
+    cityConditions.city = data.name
+    displayStats(cityConditions)
   });
 }
 
-function displayWeather(cityConditions) {
+function displayStats(cityConditions) {
   $(".weather-form").empty();
-  rainOrShine(cityConditions.weather)
-
+  var weatherResult = checkWeather(cityConditions.weather)
+  addWeather(weatherResult)
   $.each(cityConditions, function(info, value){
     $(".weather-form").append("<div class=" + info + "><p>" + value + "</p></div>")
   })
 }
 
-function rainOrShine(weather) {
+function addWeather(weatherResult) {
+  $(".weather-form").append("<div class='weather-image'> <i class=\"" + weatherResult + "\"></i></div>")
+}
+
+function checkWeather(weather) {
   if (weather == "Clouds") {
-    $(".weather-form").append('<div class="weather-image"> <i class="wi wi-cloudy"></i></div>')
+    return "wi wi-cloudy"
   } else if (weather== "Rain") {
-    $(".weather-form").append('<div class="weather-image"> <i class="wi wi-rains"></i></div>')
+    return "wi wi-rains"
   } else if (weather== "Snow") {
-    $(".weather-form").append('<div class="weather-image"> <i class="wi wi-snow"></i></div>')
+    return "wi wi-snow"
   } else {
-    $(".weather-form").append('<div class="weather-image"> <i class="wi wi-day-sunny"></i></div>')
+    return "wi wi-day-sunny"
   }
 }
